e911bd98872c3cc432b8fcecf26d4a34e2971974
Shenuka Gunathilake <shenukanimshangunathilake.mestiyagedon@students.mq.edu.au>
2025-05-12 00:54:24 +1000

    Fixed allowedhosts error

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     port: process.env.PORT || 3000,
-    host: '0.0.0.0'
+    host: '0.0.0.0',
+    allowedHosts: ['brainybucks.onrender.com'] 
   }
   ,plugins: [react()],
 })
